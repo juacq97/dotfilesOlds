1d984ae2d9987b3f9e9408109c7ec52186c72166
Juan <juacq97@tutanota.com>
2019-06-17 23:55:06 -0600

    Cambios menores

--- .config/polybar/polybar-monitors.sh
+++ .config/polybar/polybar-monitors.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
 
+#MATA POLYBAR Y LO VUELVE A CORRER. SE USA EN i3 CONFIG.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar -c ~/.config/polybar/config example -r &
   done
 else
-  polybar --reload example &
+polybar -c ~/.config/polybar/config example -r &
 fi
