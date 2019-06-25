d862140d088f63feb3edf5f1b4adb624e56e663a
Juan <juacq97@tutanota.com>
2019-06-24 21:47:33 -0600

    Cambios menores

--- scripts/get_agenda.sh
+++ scripts/get_agenda.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 emacs -batch -l ~/.emacs.d/init.el --eval "(run-hooks 'emacs-startup-hook)" --eval '(org-batch-agenda "n"
-	org-agenda-span (quote week))' | sed "/Day-agenda.*/d"
+	org-agenda-span 3)' | sed "/Day-agenda.*/d"
