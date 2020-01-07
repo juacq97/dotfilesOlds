8aba01a9fd91735df77ce39b09170d464838b926
Juan <juacq97@tutanota.com>
2020-01-07 07:43:52 -0700

    Mountains

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,6 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
-
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
--- st/config.h
+++ st/config.h
@@ -1,5 +1,4 @@
 /* See LICENSE file for copyright and license details. */
-
 /*
  * appearance
  *
