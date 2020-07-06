f4efe0b63c3d71d354e9fac781e8d52ef523b268
juacq97 <juacq97@tutanota.com>
2020-07-05 18:52:04 -0600

    backup

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /* static char *font = "Fantasque Sans Mono:pixelsize=16:antialias=true:autohint=true"; */
-static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=13:antialias=true:autohint=true";
 
 static int borderpx = 2;
 
